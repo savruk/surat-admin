b990186bd9b9b1833291acaac345adc9d653fca4
Yusuf Koseoglu (aka savruk) <yusufkoseoglu287@gmail.com>
2014-10-17 22:46:36 +0200

    before yosemite

--- surat-admin.go
+++ surat-admin.go
@@ -1,23 +1,40 @@
 package main
 
+// import (
+// 	"flag"
+
+// 	"github.com/savruk/surat"
+// )
+
+// var flush bool
+
+// func init() {
+// 	flag.BoolVar(&flush, "flush", false, "Delete all the keys!?")
+// }
+
+// func main() {
+// 	flag.Parse()
+// 	if flush {
+// 		surat.Flush()
+// 	} else {
+// 		surat.Run()
+// 	}
+
+// }
+
 import (
 	"flag"
+	"log"
+	"net/http"
 
-	"github.com/savruk/surat"
+	"github.com/elazarl/goproxy"
 )
 
-var flush bool
-
-func init() {
-	flag.BoolVar(&flush, "flush", false, "Delete all the keys!?")
-}
-
 func main() {
+	verbose := flag.Bool("v", false, "should every proxy request be logged to stdout")
+	addr := flag.String("addr", ":8080", "proxy listen address")
 	flag.Parse()
-	if flush {
-		surat.Flush()
-	} else {
-		surat.Run()
-	}
-
+	proxy := goproxy.NewProxyHttpServer()
+	proxy.Verbose = *verbose
+	log.Fatal(http.ListenAndServe(*addr, proxy))
 }
